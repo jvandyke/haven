0735e3900d51c8119138631065d028cc3a523a9b
Kheoh Yee Wei <kwei88@gmail.com>
2017-06-27 11:59:01 +0800

    removes @angular/material. creates card component and stylings

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KanbanRoutingModule } from './kanban-routing.module';
-import { ListModule } from '../components/list/index';
 import { KanbanComponent } from './kanban.component';
+import { ListComponent } from '../components/list/list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     KanbanRoutingModule,
-    ListModule
   ],
-  declarations: [KanbanComponent]
+  declarations: [
+    KanbanComponent,
+    ListComponent
+  ]
 })
 export class KanbanModule { }
